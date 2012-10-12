d73caaf4bfbce8c819200af67a89dac0973a338b
Lew Parker <parker@earthvectors.com>
2012-10-12 15:44:38 -0600

    Fix RGB LED component to turn on the right colors

--- examples/rgb_led.rb
+++ examples/rgb_led.rb
@@ -6,17 +6,34 @@
 require '../lib/dino'
 
 board = Dino::Board.new(Dino::TxRx.new)
-led = Dino::Components::RgbLed.new(pins: {red: 9, green: 10, blue: 11}, board: board)
+led = Dino::Components::RgbLed.new(pins: {red: 11, green: 10, blue: 9}, board: board)
+potentiometer = Dino::Components::Sensor.new(pin: 'A0', board: board)
 
-sleep(2)
-led.red
-sleep(1)
-led.blue
-sleep(1)
-led.green
-sleep(1)
-led.color(224, 27, 106)
-sleep(1)
-led.color(255, 149, 7)
-sleep(1)
-led.off
+
+delay = 500.0
+
+set_delay = Proc.new do |data|
+  sleep 0.5
+  puts "DATA: #{delay = data.to_i}"
+end
+
+potentiometer.when_data_received(set_delay)
+
+  sleep(2)
+loop do
+  puts "DELAY: #{seconds = (delay / 1000.0)}"
+  p 'red'
+  led.red
+  sleep(seconds)
+  led.blue
+  p 'blue'
+  sleep(seconds)
+  led.green
+  p 'green'
+  sleep(seconds)
+end
+#led.color(224, 27, 106)
+#sleep(1)
+#led.color(255, 149, 7)
+#sleep(1)
+#led.off
