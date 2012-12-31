8c88bf328ce301530cb8e5b860704e23f4c3fcd0
Vickash Mahabir <vickashmahabir@gmail.com>
2012-12-31 16:16:07 -0400

    Added a TxRx for Telnet connections and an example

---
+++ lib/dino/tx_rx/serial.rb
@@ -0,0 +1,69 @@
+require 'serialport'
+require 'observer'
+
+module Dino
+  module TxRx
+    class Serial
+      include Observable
+
+      BAUD = 115200
+
+      def initialize
+        @first_write = true
+      end
+
+      def io
+        raise BoardNotFound unless @io ||= find_arduino
+        @io
+      end
+
+      def io=(device)
+        @io = SerialPort.new(device, BAUD)
+      end
+
+      def read
+        @thread ||= Thread.new do
+          loop do
+            if IO.select([io], nil, nil, 0.005)
+              pin, message = *io.gets.chop.split(/::/)
+              pin && message && changed && notify_observers(pin, message)
+            end
+            sleep 0.004
+          end
+        end
+      end
+
+      def close_read
+        return nil if @thread.nil?
+        Thread.kill(@thread)
+        @thread = nil
+      end
+
+      def write(message)
+        IO.select(nil, [io], nil)
+        io.puts(message)
+      end
+
+      private
+
+      def tty_devices
+        if RUBY_PLATFORM.include?("mswin") || RUBY_PLATFORM.include?("mingw")
+          ["COM1", "COM2", "COM3", "COM4"]
+        else
+          `ls /dev`.split("\n").grep(/usb|ACM/).map{|d| "/dev/#{d}"}
+        end
+      end
+
+      def find_arduino
+        tty_devices.map do |device|
+          next if device.match /^cu/
+          begin
+            SerialPort.new(device, BAUD)
+          rescue
+            nil
+          end
+        end.compact.first
+      end
+    end
+  end
+end
