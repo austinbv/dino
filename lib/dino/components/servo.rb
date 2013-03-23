e7ff676863bc00da8759bffa4fa59a0f056cd46b
Vickash Mahabir <vickashmahabir@gmail.com>
2013-03-23 14:10:56 -0400

    Fix tests

--- lib/dino/components/servo.rb
+++ lib/dino/components/servo.rb
@@ -6,12 +6,11 @@ module Dino
       def after_initialize(options={})
         set_pin_mode(:out)
         board.servo_toggle(pin, 1)
-        position = options[:position] || 0
+        self.position = options[:position] || 0
       end
 
       def position=(value)
-        board.servo_write(pin, angle(value))
-        @position = value
+        board.servo_write(pin, @position = angle(value))
       end
 
       def angle(value)
