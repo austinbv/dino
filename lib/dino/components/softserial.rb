4805ad26c5035475b8c8d74908fef837de688d91
Christian Carter <cdcarter@gmail.com>
2013-07-15 22:40:37 -0400

    initial component class for the softwareserial

---
+++ lib/dino/components/softserial.rb
@@ -0,0 +1,39 @@
+module Dino
+  module Components
+    class SoftwareSerial < Core::MultiPin
+      COMMAND = 12
+      # Initialize
+      #
+      # Dino::Components::SoftwareSerial.new(
+      #       board: board,
+      #       pins: { rx:10, tx:11 },
+      #       baud: 9600
+      # )
+      #
+      def after_initialize(options)
+        board.write Dino::Message.encode command: COMMAND, value: 0, aux_message: encoded_pins
+        board.write Dino::Message.encode command: COMMAND, value: 1, aux_message: baud
+      end
+      
+      # A useful pattern to be implemented if needed
+      # LIBRARY_COMMANDS = {
+      # }
+
+      # LIBRARY_COMMANDS.each_pair do |command, command_id|
+      #   define_method(command) do
+      #     board.write Dino::Message.encode(command: 13, value: command_id)
+      #   end
+      # end
+
+      def puts(string)
+        board.write Dino::Message.encode command: COMMAND, value: 3, aux_message: string
+      end
+
+      private
+
+      def encoded_pins
+        [pins[:rx], pins[:tx]].compact.join(',')
+      end
+    end
+  end
+end
