5de1fdbf8fcbab27c46d0549720c2bd88ecbad5e
Vickash Mahabir <vickashmahabir@gmail.com>
2013-10-08 11:15:44 -0400

    Fix a typo

--- lib/dino/components/hcsr04.rb
+++ lib/dino/components/hcsr04.rb
@@ -2,7 +2,7 @@ module Dino
   module Components
     class HCSR04 < Core::BaseInput
       def _read
-        board.ulstrasonic_read(self.pin)
+        board.ultrasonic_read(self.pin)
       end
     end
   end
