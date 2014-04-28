5ef757005eeed9a80f4e213b27d62e9214f4ab86
MinasMazar <minasmazar@gmail.com>
2014-04-28 02:09:27 +0200

    Add perform click function for button component

--- lib/dino/components/button.rb
+++ lib/dino/components/button.rb
@@ -3,6 +3,10 @@ module Dino
     class Button < Basic::DigitalInput
       alias :down :on_low
       alias :up   :on_high
+      
+      def perform_click
+          update HIGH
+          update LOW
     end
   end
 end
