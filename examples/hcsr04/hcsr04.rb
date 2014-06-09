47804bafe1f30a1dc2ca037115a8a3ff422b56ca
Vickash Mahabir <vickashmahabir@gmail.com>
2014-06-08 21:05:46 -0400

    Updated HCSR04 component to use mixins. Added proper example.

--- examples/hcsr04/hcsr04.rb
+++ examples/hcsr04/hcsr04.rb
@@ -1,12 +1,17 @@
 #
-# This is an example of how to use the servo class
+# This is an example of how to use the HCSR04 ultrasonic distance sensor.
 #
 require 'bundler/setup'
 require 'dino'
 
 board = Dino::Board.new(Dino::TxRx::Serial.new)
-sensor = Dino::Components::HCSR04.new(pin: 9, board: board)
+sensor = Dino::Components::HCSR04.new(pin: 52, board: board)
 
-sensor.read do |reading|
-  puts reading
+# Read the distance every 5 seconds.
+sensor.poll(5) do |reading|
+  # Convert microseconds to inches
+  inches = (reading.to_f / (2 * 73.746)) 
+  puts "#{inches} in."
 end
+
+sleep
--- lib/dino/components/hcsr04.rb
+++ lib/dino/components/hcsr04.rb
@@ -1,6 +1,10 @@
 module Dino
   module Components
-    class HCSR04 < Core::BaseInput
+    class HCSR04
+      include Setup::SinglePin
+      include Setup::Input
+      include Mixins::Poller
+
       def _read
         board.ultrasonic_read(self.pin)
       end
