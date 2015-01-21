f4b5d357095a3330e85d083e94e09fd164ffdc6a
Kouji Takao <kouji.takao@gmail.com>
2015-01-21 12:24:52 +0900

    removed requring button.

--- lib/smalrubot/components.rb
+++ lib/smalrubot/components.rb
@@ -2,7 +2,6 @@ module Smalrubot
   module Components
     require 'smalrubot/components/base_component'
     autoload :Led,        'smalrubot/components/led'
-    autoload :Button,     'smalrubot/components/button'
     autoload :Sensor,     'smalrubot/components/sensor'
     autoload :Servo,      'smalrubot/components/servo'
   end
