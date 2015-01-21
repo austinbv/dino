06a2301978bf6712030eff50b2052752b253df80
Kouji Takao <kouji.takao@gmail.com>
2015-01-21 12:25:24 +0900

    added example code.

--- examples/test.rb
+++ examples/test.rb
@@ -3,11 +3,23 @@ require 'smalrubot'
 Smalrubot.debug_mode = true
 board = Smalrubot::Board.new(Smalrubot::TxRx::Serial.new)
 
+=begin
 board.digital_write(13, 255)
 sleep(1)
 board.digital_write(13, 0)
+=end
 
+=begin
 10.times do
   p board.analog_read(0)
   sleep(0.5)
 end
+=end
+
+=begin
+10.times do
+  p board.digital_read(3)
+  p board.digital_read(4)
+  sleep(0.5)
+end
+=end
