6259c3a345e27dc836e17681a0003936a9421741
Kouji Takao <kouji.takao@gmail.com>
2015-01-21 21:40:31 +0900

    removed sensor from autoload.

--- lib/smalrubot/components.rb
+++ lib/smalrubot/components.rb
@@ -2,7 +2,6 @@ module Smalrubot
   module Components
     require 'smalrubot/components/base_component'
     autoload :Led,        'smalrubot/components/led'
-    autoload :Sensor,     'smalrubot/components/sensor'
     autoload :Servo,      'smalrubot/components/servo'
   end
 end
