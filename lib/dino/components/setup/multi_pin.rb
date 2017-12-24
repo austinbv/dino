48a60eabe50bc0c833294c3c8497587e4f6fd1e1
vickash <vickashmahabir@gmail.com>
2017-12-24 15:50:44 -0400

    Clearer comments for how multi pin components work.

--- lib/dino/components/setup/multi_pin.rb
+++ lib/dino/components/setup/multi_pin.rb
@@ -3,7 +3,10 @@ module Dino
     module Setup
       module MultiPin
         #
-        # Build complex components by modeling them as separate single pin subcomponents.
+        # Model complex components by allowing them to use multiple pins.
+        # Pins may be "required" or not, and "proxied" or not.
+        # Proxying a pin creates a single-pin component on that pin of the given
+        # class and stores it in @proxies.
         #
         include Base
         attr_reader :pin, :pins, :pullups, :proxies
@@ -26,23 +29,14 @@ module Dino
           base.extend ClassMethods
         end
 
-        #
-        # A subcomponent's pin can be required, proxied, or both.
-        #
-        # Requiring a pin simply raises an error if a value for it is not specified in the options[:pins] hash.
-        # It will NOT automatically set up a subcomponent or do anything further.
-        # This can be useful for native libraries where modeling the pin as a subcomponent is not necessary.
-        # Or for components with pins that may be left disconnected.
-        #
-        # Proxying a pin automatically requires it, and specifies what single-pin component it should be modeled as.
-        # This module will automatically intialize the proxy component and store it in the @proxies instance variable.
-        # It also creates an attr_accessor on the singleton class for the subcomponent.
-        # The accessor and the hash element are both named using the pin's key from the options[:pins] hash.
-        #
-        # A proxied pin can be NOT required by including `optional: true` in the options hash passed to #proxy_pins.
-        # Please see the source for the SSD and RgbLed components for good examples of how all this works.
-        #
         module ClassMethods
+          #
+          # Requiring a pin simply raises an error if not specified in options[:pins].
+          # It will not automatically set up a subcomponent or do anything else.
+          # This is useful for calling Arduino-native libraries, where we need
+          # to say what pins we are using, but don't need to do more in Ruby.
+          # See the LCD component for an example.
+          #
           def require_pins(*args)
             required_pins = self.class_eval('@@required_pins') rescue []
             required_pins = (required_pins + args).uniq
@@ -50,6 +44,22 @@ module Dino
           end
           alias :require_pin :require_pins
 
+          #
+          # Proxying a pin models it as a single-pin component and requires it.
+          # It can be made optional by adding `optional: true` to the hash.
+          #
+          # On initializing a multi-pin component, its proxy instances are
+          # created and stored in the @proxies variable. Call #proxies for access.
+          #
+          # Lastly, convenience methods for each proxy are defined on the singleton
+          # class of the multi-pin instance.
+          #
+          # For example, you can call `rgb_led.red` for the AnalogOutput
+          # instance that controls the red part of that RGB led.
+          #
+          # These method names correspond to the hash keys (pin names) defined in
+          # the class definition call to `proxy_pins`. See RgbLed class for examples.
+          #
           def proxy_pins(options={})
             if options[:optional]
               options.reject! { |k| k == :optional }
