f4b01e0f38579e679dd06e3c491ca1dab6caba6b
vickash <vickashmahabir@gmail.com>
2018-02-13 16:21:50 -0400

    Trying to fix jruby tests

--- spec/lib/components/mixins/threaded_spec.rb
+++ spec/lib/components/mixins/threaded_spec.rb
@@ -45,67 +45,72 @@ module Dino
 
           it 'should store in @thread' do
             thread = Thread.current
-            async = Proc.new { thread = Thread.current }
-            subject.threaded(&async)
-            while(!subject.instance_variable_get :@thread) do; end
-            expect(subject.instance_variable_get :@thread).to_not eq(thread)
+            component = subject
+            component.threaded {}
+            while(!component.instance_variable_get :@thread) do; end
+            expect(component.instance_variable_get :@thread).to_not eq(thread)
           end
         end
 
         describe '#threaded_loop' do
           it 'should loop the block in the thread' do
+            component = subject
             main_thread = Thread.current
             async_thread = Thread.current
             async = Proc.new { async_thread = Thread.current}
 
             expect(async).to receive(:call).and_call_original
-            expect(subject).to receive(:loop) do |&block|
+            expect(component).to receive(:loop) do |&block|
               expect(block).to eq(async)
             end.and_yield
 
-            subject.threaded_loop(&async)
+            component.threaded_loop(&async)
             while(main_thread == async_thread) do; end
-            subject.stop_thread
+            component.stop_thread
             expect(main_thread).to_not eq(async_thread)
           end
         end
 
         describe '#stop_thread' do
           it 'should kill the thread' do
-            subject.threaded { sleep }
-            expect(subject.instance_variable_get :@thread).to receive(:kill)
-            subject.stop_thread
+            component = subject
+            component.threaded { sleep }
+            expect(component.instance_variable_get :@thread).to receive(:kill)
+            component.stop_thread
           end
         end
 
         describe '#enable_interrupts' do
           it 'should override the given method on the singleton class only' do
+            first_part = subject
             second_part = ThreadedComponent.new(board: board)
             before_class = second_part.method(:foo)
-            before_instance = subject.method(:foo)
+            before_instance = first_part.method(:foo)
 
-            subject.enable_interrupts
+            first_part.enable_interrupts
             after_class = second_part.method(:foo)
-            after_instance = subject.method(:foo)
+            after_instance = first_part.method(:foo)
 
             expect(after_class).to eq(before_class)
             expect(after_instance).to_not eq(before_instance)
           end
 
           it 'should pass arguments through to the original method' do
-            original = subject.method(:foo)
-            subject.enable_interrupts
-            subject.foo("dino")
-            expect(original).to_not eq(subject.method(:foo))
-            expect(subject.bar).to eq("dino")
+            component = subject
+            original = component.method(:foo)
+            component.enable_interrupts
+            component.foo("dino")
+            expect(original).to_not eq(component.method(:foo))
+            expect(component.bar).to eq("dino")
           end
         end
 
         describe 'calling an interrupt' do
           it 'should stop the thread' do
-            subject.threaded { sleep }
-            expect(subject).to receive(:stop_thread)
-            subject.foo
+            component = subject
+            component.threaded { sleep }
+            expect(component).to receive(:stop_thread)
+            component.foo
           end
         end
       end
