1694632863700cc5c1c014395336527a11f686b6
vickash <vickashmahabir@gmail.com>
2018-02-15 11:57:00 -0400

    More 1-Wire refactoring

--- lib/dino/components/one_wire/ds18b20.rb
+++ lib/dino/components/one_wire/ds18b20.rb
@@ -2,28 +2,28 @@ module Dino
   module Components
     module OneWire
       class DS18B20 < Slave
-
         FAMILY_CODE = 0x28
 
+        def scratch
+          @state ? @state[:raw] : read_scratch(9)
+        end
+
         def resolution
-          @resolution ||= decode_resolution read_scratch(9)
+          @resolution ||= decode_resolution(scratch)
         end
 
         def resolution=(bits)
-          raise ArgumentError 'Invalid DS18B20 resolution' if (bits > 12 || bits < 9)
-
-          @resolution = bits
-          scratch = read_scratch(9).split(",").map(&:to_i)
-
-          unless decode_resolution(scratch) == @resolution
-            settings = scratch[2..4]
-            offset = @resolution - 9
-            settings[2] = 0b00011111 | (offset << 5)
-            write_scratch(settings)
-            copy_scratch
+          unless (9..12).include?(bits)
+            raise ArgumentError, 'Invalid DS18B20 resolution, expected 9 to 12'
           end
 
-          set_convert_time
+          return bits if decode_resolution(scratch) == bits
+
+          eeprom = scratch[2..4]
+          eeprom[2] = 0b00011111 | ((bits - 9) << 5)
+          write_scratch(eeprom)
+          copy_scratch
+          @resolution = bits
         end
 
         def set_convert_time
@@ -44,14 +44,11 @@ module Dino
 
         def _read
           convert
-          read_scratch(9)
+          read_scratch(9) { |data| self.update(data) }
         end
 
-        def pre_callback_filter(data)
-          # Data is 9 comma delimited numbers in ASCII, representing bytes.
-          bytes = data.split(",").map{ |b| b.to_i }
-          return {crc_error: true} unless Helper.crc_check(bytes)
-
+        def pre_callback_filter(bytes)
+          return { crc_error: true } unless Helper.crc_check(bytes)
           @resolution = decode_resolution(bytes)
 
           decode_temp(bytes).merge(raw: bytes)
@@ -59,23 +56,10 @@ module Dino
 
         #
         # Temperature is the first 16 bits (2 bytes of 9 read), little-endian.
-        # It's a sign-extended two's complement 12-bit decimal, where LSB is the
-        # 2^-4 exponent, up through 2^6 for next 10 bits. 5 MSBs repeat the sign.
+        # It's a signed, 2's complement, little-endian decimal with 2^-4 exp.
         #
         def decode_temp(bytes)
-          magnitude = bytes[1] << 8 | bytes[0]
-          negative = (magnitude[15] == 1)
-          magnitude = (magnitude ^ 0xFFFF) + 1 if negative
-
-          celsius = 0.0
-          exp = -4
-          for bit in (0..10)
-            if (magnitude[bit] == 1)
-              celsius = celsius + (2.0 ** (exp+bit))
-            end
-          end
-
-          celsius = -celsius if negative
+          celsius = bytes[0..1].pack('C*').unpack('s<')[0] * (2.0 ** -4)
           farenheit = (celsius * 1.8 + 32).round(4)
 
           {celsius: celsius, farenheit: farenheit}
--- lib/dino/components/one_wire/slave.rb
+++ lib/dino/components/one_wire/slave.rb
@@ -14,19 +14,26 @@ module Dino
 
         def after_initialize(options={})
           super(options)
+
           unless options[:address]
-            raise ArgumentError, 'missing 1-Wire slave ROM address; try searching first'
+            raise ArgumentError,
+                  'missing 1-Wire slave ROM address; try Bus#search first'
           end
           @address = options[:address]
         end
 
-        def read_scratch(num_bytes)
+        def read_scratch(num_bytes, &block)
           atomically do
-            # Bubble a single bus callback up to self, bound by this lock.
-            bus.add_callback(:read) { |data| self.update(data) }
+            # Bubble bus callback while still in the lock.
+            if block_given?
+              bus.add_callback(:read) do |data|
+                block.call data.split(",").map(&:to_i)
+              end
+            end
+
             match
             bus.write(READ_SCRATCH)
-            bus.read(num_bytes)
+            bus.read(num_bytes).split(",").map(&:to_i)
           end
         end
 
@@ -42,7 +49,7 @@ module Dino
           atomically do
             match
             bus.write(COPY_SCRATCH)
-            sleep 0.02
+            sleep 0.05
             bus.reset if bus.parasite_power
           end
         end
@@ -59,8 +66,12 @@ module Dino
             bus.write(SKIP_ROM)
           else
             bus.write(MATCH_ROM)
-            bus.write(Helper.address_to_bytes(self.address))
+            bus.write(address_bytes)
           end
+        end
+
+        def address_bytes
+          Helper.address_to_bytes(self.address)
         end
 
         def serial_number
@@ -68,10 +79,8 @@ module Dino
         end
 
         def extract_serial
-          # Remove CRC.
-          address = @address & 0x00FFFFFFFFFFFFFF
-          # Remove family code.
-          address = address >> 8
+          # Remove CRC & family code.
+          address = (@address & 0x00FFFFFFFFFFFFFF) >> 8
           address.to_s(16).rjust(12, "0")
         end
       end
