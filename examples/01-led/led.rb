be19bb3a3734c27c4bd047eaa77393f030fd23f7
vickash <vickashmahabir@gmail.com>
2023-02-15 01:54:08 -0400

    Update button example

--- examples/01-led/led.rb
+++ examples/01-led/led.rb
@@ -1,34 +1,39 @@
 #
-# Simple example that controls an LED.
+# Example 1: Controlling an LED
 #
 require 'bundler/setup'
 require 'dino'
 
-# If the board is connected with a USB cable, it's most likely serial over USB.
-connection = Dino::TxRx::Serial.new
+# If the board is plugged into a USB port, we can talk to it with serial over USB.
+io = Dino::TxRx::Serial.new
 
-# Initialize an object to represent the board, giving the connection.
-board = Dino::Board.new(connection)
-
-# Initialize an object representing the LED, giving the board and pin.
-# The on-board LED (marked "L") is connectd to pin 13 on most Arduinos.
-led = Dino::Components::Led.new(pin: 13, board: board)
+# Create an object to represent the board, giving the I/O object.
+board = Dino::Board.new(io)
 
 #
-# Now we can control the LED.
-# Led is a DigitalOutput, so it can only have one of two states:
+# Create an object for the LED, giving the board, and the pin that the positive
+# leg of your LED is connected to. The longer leg is usually positive.
+# See led.png in this folder for a hook-up diagram. 
+#
+# The on-board LED (marked "L") is internally connected to pin 13 on most Arduinos,
+# and can be used without connecting anything.
+#
+led = Dino::Components::Led.new(board: board, pin: 13)
+
+#
+# Now we can use it. A digital output can only have one of two states:
 # 1 / high / on
 # 0 / low / off
 #
-# We can write the state directly, or use named convenience methods.
-# The 3 lines below all do the same thing: turn it on, wait half a second, turn it off.
+# We can write state directly, or use named convenience methods that led has.
+# The 3 lines below all do the same thing: turn on, wait half a second, turn off.
 #
 led.write(1); sleep 0.5; led.write(0)
 led.high;     sleep 0.5; led.low
 led.on;       sleep 0.5; led.off
 
 #
-# Calling #toggle will set it to the opposite of its current state.
+# led.toggle will set it to the opposite of its current state.
 # Keep it blinking for 3 more seconds using #toggle.
 #
 6.times do
@@ -40,18 +45,18 @@ end
 # What if we want to blink in the background?
 #
 # led.blink runs in a separate thread, managed by the led object,
-# and doesn't block the main thread. Give it the blink interval in seconds.
+# and doesn't block the main thread. Give the blink interval in seconds.
 #
 led.blink 0.5
 
 # Wait while it blinks for 5 seconds.
 sleep 5
 
-# Call a method that sets the state (#write, #high, #low, #on, #off) to
-# automatically stop the blink thread.
+# Calling a method that sets the state (#write, #high, #low, #on, #off)
+# automatically stops the blink thread.
 led.off
 sleep 2
 
 # Blink faster indefinitely.
-led.blink 0.25
+led.blink 0.05
 sleep
--- examples/02-button/button.rb
+++ examples/02-button/button.rb
@@ -1,24 +1,62 @@
 #
-# This is an example of how to use the button class
-# You must register helpers and have the main thread
-# sleep or in someway keep running or your program
-# will exit before any callbacks can be called
+# Example 2: Using a Button
 #
 require 'bundler/setup'
 require 'dino'
 
+# Set up the board, connecting with serial over USB
 board = Dino::Board.new(Dino::TxRx::Serial.new)
-button = Dino::Components::Button.new(pin: 13, board: board)
 
-button.down do
-  puts "button down"
+#
+# Create an object for a momentary button, giving the board, and the pin that
+# the ungrounded side of the button is connected to.
+#
+# `pullup: true` tells the board to keep the input pin high (logical 1) when the
+# button is not pressed. Without this (or an external pullup resistor), the pin
+# might float between 0 and 1, giving incorrect readings.
+#
+# See button.png in this folder for a hook-up diagram.
+#
+button = Dino::Components::Button.new(board: board, pin: 7, pullup: true)
+
+#
+# As soon as a Button (or any DigitalInput) is created, the board starts
+# listening for changes to the physical button. When that happens, our button
+# object is notified. To catch these notifications, we have to use a callback.
+#
+# button.add_callback saves a block of code, and runs it when the button state changes.
+# 
+# The callback below checks if the state is 0 (the button went from up to down),
+# then counts and prints the number of times pressed.
+#
+presses = 0
+button.add_callback(:count_presses) do |state|
+  if state == 0
+    presses = presses + 1
+    puts "Button press ##{presses}"
+  end
 end
 
-button.up do
-  puts "button up"
-end
+# Wait for the button to be pressed 3 times.
+sleep 0.005 while presses < 3
 
-# Force callbacks to run at least once for the initial state.
-button.read
+# Callbacks are stored in a Hash. We can be specific and use keys when adding and removing.
+button.remove_callbacks(:count_presses)
+
+# Or remove them all. Either way, the block above won't run anymore.
+button.remove_callbacks
+
+#
+# button.down and button.up add callbacks that automatically check state for you.
+#
+# #down runs only when state goes from high (1) to low (0).
+# #up runs only when state goes from low (0) to high (1).
+#
+# We can use them to control the internal LED from example 1.
+#
+led = Dino::Components::Led.new(board: board, pin: 13)
+
+button.up   { led.off } 
+button.down { led.on }
 
 sleep
