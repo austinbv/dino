9a07f99ef4ebb5770ac59f7ada47e06b38396a63
vickash <vickashmahabir@gmail.com>
2023-03-25 14:55:33 -0400

    Clean up LEDArray.cpp

--- src/lib/DinoLEDArray.cpp
+++ src/lib/DinoLEDArray.cpp
@@ -12,22 +12,27 @@
   #include <Adafruit_NeoPixel.h>
 #endif
 
+//
 // CMD = 19
-// Will become a general function later. Just WS2812 for now.
+// Write data to a WS2812 LED array. Will generalize to other types later.
+//
+// pin          = Microcontroller pin connected to Data In pin of the LED array.
+// val          = Number of RGB or RGBW LEDS (aka pixels) in the array.
+// auxMsg[0..3] = Reserved for future settings.
+// auxMsg[4+]   = Raw pixel data, already in correct byte order (GRB, RGB, etc.).
+//
 void Dino::showLEDArray() {
-  // pin is the output pin, connected to Data In on the WS2812.
-  // val is the number of pixels in the array.
+  // Setup a new LED array object.
   Adafruit_NeoPixel ledArray(val, pin, NEO_GRB + NEO_KHZ800);
   ledArray.begin();
 
-  // LED data starts at auxMsg[4]. 0..3 left free for future config.
   // Copy LED data into the pixel buffer.
   // NOTE: val * 3 will have to change to val * 4 for RGBW leds.
   memcpy(ledArray.getPixels(), &auxMsg[4], val * 3);
 
-  // I have NO idea what causes the first green value to stay stuck on 255,
-  // but a small delay here seems like a consistent fix. Different
-  // values for each platform. ESP32 doesn't seem to need it.
+  // I have NO idea what causes the first green LED to stay stuck on.
+  // A small delay here seems like a consistent fix. ESP32 doesn't need it.
+  // AVR can get away with 32us, but others need 64.
   #if defined(ESP8266) || defined(__SAMD21G18A__) || defined(__SAM3X8E__)
     delayMicroseconds(64);
   #endif
