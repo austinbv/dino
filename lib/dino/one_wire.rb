e008c797ff5222ce3034d47d2a7bd571961b333f
vickash <vickashmahabir@gmail.com>
2023-03-31 13:07:15 -0400

    Fix one wire module typo

--- lib/dino/one_wire.rb
+++ lib/dino/one_wire.rb
@@ -1,7 +1,7 @@
 module Dino
   module OneWire
     autoload :Constants,      "#{__dir__}/one_wire/constants"
-    autoload :Helper,         "#{__dir__}/one_wire/Helper"
+    autoload :Helper,         "#{__dir__}/one_wire/helper"
     autoload :BusEnumeration, "#{__dir__}/one_wire/bus_enumeration"
     autoload :Bus,            "#{__dir__}/one_wire/bus"
     autoload :Peripheral,     "#{__dir__}/one_wire/peripheral"
